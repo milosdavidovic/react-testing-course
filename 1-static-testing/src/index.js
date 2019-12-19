9b951b31ec4558048beb5b0f50aa9c679b446e82
milosdavidovic <milosgrozni@gmail.com>
2019-12-19 13:03:22 +0100

    added example file

---
+++ 1-static-testing/src/index.js
@@ -0,0 +1,11 @@
+const firstName = "John"
+const lastName = 'Smith';
+
+lastName = 'Doe';
+
+
+function greeter(firstName,  lastName) {
+   console.log("Hello " + firstName + " " +  lastName)}
+
+
+greeter(firstName,  lastName);
