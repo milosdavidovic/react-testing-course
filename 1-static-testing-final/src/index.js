383481f31e7d8127ec33f187a30acc50d6694312
milosdavidovic <milosgrozni@gmail.com>
2019-12-27 08:28:08 +0100

    added and configured eslint

--- 1-static-testing-final/src/index.js
+++ 1-static-testing-final/src/index.js
@@ -1,11 +1,9 @@
-const firstName = "John"
-const lastName = 'Smith';
+const firstName = "John";
+let lastName = "Smith";
+lastName = "Doe";
 
-lastName = 'Doe';
+function greeter(firstName, lastName) {
+  console.log(`Hello ${firstName} ${lastName}`);
+}
 
-
-function greeter(firstName,  lastName) {
-   console.log("Hello " + firstName + " " +  lastName)}
-
-
-greeter(firstName,  lastName);
+greeter(firstName, lastName);
