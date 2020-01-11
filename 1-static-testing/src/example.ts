14959b46b752f46dc6c74c1b82cd9ba5fbdccb5c
milosdavidovic <milosgrozni@gmail.com>
2020-01-11 13:05:58 +0100

    ts example added

---
+++ 1-static-testing/src/example.ts
@@ -0,0 +1,25 @@
+interface Address {
+  houseNumber: number;
+  streetName: string;
+  city: string;
+}
+
+const addresses: Address[] = [
+  {
+    houseNumber: 2,
+    streetName: "Novosadskog sajma",
+    city: "Novi Sad"
+  },
+  {
+    houseNumber: 15,
+    streetName: "Kralja Aleksandra",
+    city: "Zrenjanin"
+  }
+];
+
+const filterByStreetName = (input: Address[], searchTerm: string) => {
+  // what if streetName is undefined?
+  return input ? input.filter(a => a.streetName.indexOf(searchTerm) > -1) : [];
+};
+
+filterByStreetName(addresses, "Novosad");
