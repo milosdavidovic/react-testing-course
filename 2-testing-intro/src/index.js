c316e6b86e94b6b405825cb5dbf39ca7c28ee096
milosdavidovic <milosgrozni@gmail.com>
2020-01-14 19:57:49 +0100

    created simple expect utility

--- 2-testing-intro/src/index.js
+++ 2-testing-intro/src/index.js
@@ -9,16 +9,24 @@ function square(a) {
 // In another file...
 // import {multiply, square} from "./math"
 
+function expect(actual) {
+  return {
+    toBe(expected) {
+      if (expected != actual) {
+        throw new Error(`${expected} is not equal to ${actual}`);
+      }
+    },
+    toBeGreaterThen(value) {},
+    toBeDefined() {}
+  };
+}
+
 let expectedResult = 16;
 let actualResult = square(4);
-if (expectedResult != actualResult) {
-  throw new Error(`${expectedResult} is not equal to ${actualResult}`);
-}
+expect(actualResult).toBe(expectedResult);
 
 expectedResult = 8;
 actualResult = multiply(2, 4);
-if (expectedResult != actualResult) {
-  throw new Error(`${expectedResult} is not equal to ${actualResult}`);
-}
+expect(actualResult).toBe(expectedResult);
 
 console.log("Tests passed!");
