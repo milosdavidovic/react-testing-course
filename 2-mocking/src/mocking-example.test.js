871bb9d70f3129af70188a0a772e6ee50ecd1db5
milosdavidovic <milosgrozni@gmail.com>
2020-01-25 11:28:53 +0100

    monkey patching - cleanup

--- 2-mocking/src/mocking-example.test.js
+++ 2-mocking/src/mocking-example.test.js
@@ -3,9 +3,12 @@ const coinFlipper = require("./utils/coinFlipper");
 
 test("when user wins a bet, then takeABet will return true", () => {
   // arrange (monkey patching)
+  const flipACoinOriginal = coinFlipper.flipACoin;
   coinFlipper.flipACoin = () => "heads";
   // act
   const result = takeABet("1", "heads", 100);
   // assert
   expect(result).toBe(true);
+  //clean-up
+  coinFlipper.flipACoin = flipACoinOriginal;
 });
