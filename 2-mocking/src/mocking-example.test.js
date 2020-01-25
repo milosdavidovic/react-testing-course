f745e5510de41f1b65f86b759f715a38d8bc588a
milosdavidovic <milosgrozni@gmail.com>
2020-01-25 11:38:40 +0100

    added another test

--- 2-mocking/src/mocking-example.test.js
+++ 2-mocking/src/mocking-example.test.js
@@ -12,3 +12,15 @@ test("when user wins a bet, then takeABet will return true", () => {
   //clean-up
   coinFlipper.flipACoin = flipACoinOriginal;
 });
+
+test("when user looses a bet, then takeABet will return false", () => {
+  // arrange (monkey patching)
+  const flipACoinOriginal = coinFlipper.flipACoin;
+  coinFlipper.flipACoin = () => "tails";
+  // act
+  const result = takeABet("1", "heads", 100);
+  // assert
+  expect(result).toBe(false);
+  //clean-up
+  coinFlipper.flipACoin = flipACoinOriginal;
+});
