3b4c8ac0fb96a15698a88c6c69659c214ed1d09f
milosdavidovic <milosgrozni@gmail.com>
2020-01-25 11:16:43 +0100

    wrote flaky test

--- 2-mocking/src/mocking-example.test.js
+++ 2-mocking/src/mocking-example.test.js
@@ -1,2 +1,8 @@
 const takeABet = require("./mocking-example");
 const coinFlipper = require("./utils/coinFlipper");
+
+test("this is one flaky test", () => {
+  const result = takeABet("1", "heads", 100);
+
+  expect(result).toBe(true);
+});
