511ea3d2e56ddcfec9b0b32555df4ff3e8a70966
milosdavidovic <milosgrozni@gmail.com>
2020-01-25 11:27:57 +0100

    monkey patching

--- 2-mocking/src/mocking-example.test.js
+++ 2-mocking/src/mocking-example.test.js
@@ -1,8 +1,11 @@
 const takeABet = require("./mocking-example");
 const coinFlipper = require("./utils/coinFlipper");
 
-test("this is one flaky test", () => {
+test("when user wins a bet, then takeABet will return true", () => {
+  // arrange (monkey patching)
+  coinFlipper.flipACoin = () => "heads";
+  // act
   const result = takeABet("1", "heads", 100);
-
+  // assert
   expect(result).toBe(true);
 });
